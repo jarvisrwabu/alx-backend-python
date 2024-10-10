3b422e104287e0c9a52f5623e293831a2bf6ddb4
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-10 15:18:19 +0300

    Use module List

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Function that returns sum of floating point nums in list."""
+from typing import List
 
 
-def sum_list(a: list[float]) -> float:
+def sum_list(a: List[float]) -> float:
     """Return sum of all the floating point numbers."""
     s: float = 0.0
     for num in a:
