1bb9b0b2e8d4e35a8912624a63dab6e8b48a60b6
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-10 16:04:29 +0300

    Return a tuple of inputs

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function that returns tuple of inputs."""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+    """Return tuple of inputs."""
+    return (k, v)
