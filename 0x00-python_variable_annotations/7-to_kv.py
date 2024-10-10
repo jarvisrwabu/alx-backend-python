9e09de2cfe8fbdcc0041cf4299088c347d8e4875
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-10 16:07:50 +0300

    Fix code

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -3,6 +3,6 @@
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Return tuple of inputs."""
-    return (k, v)
+    return (k, v**2)
