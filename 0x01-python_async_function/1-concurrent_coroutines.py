ac90bd6f6c770f4087d6cec5327468435e9431d3
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 08:25:20 +0300

    Sort output us ung sorted()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -11,11 +11,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         delay = await basic_async.wait_random(max_delay)
         delays.append(delay)
 
-    # Ensure delays are in ascending order as they are completed
-    sorted_delays = []
-    while delays:
-        min_delay = min(delays)
-        sorted_delays.append(min_delay)
-        delays.remove(min_delay)
-
-    return sorted_delays
+    return sorted(delays)
