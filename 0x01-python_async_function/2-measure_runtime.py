a003d676f963c725eafb5da9804fb959848a6f73
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 12:08:34 +0300

    Fix logic errors

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """Function that gets time taken to complete the program."""
 import time
-conc = __import__("1-concurrent_coroutines")
+wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """Get time taken to complete program."""
     # Save timestamp
     start = time.time()
 
-    await conc.wait_n(n, max_delay)
+    wait_n(n, max_delay)
 
     # Save timestamp
     end = time.time()
