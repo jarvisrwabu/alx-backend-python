46cec83f5fbfe6ef4fdb5c161bb0aa7cfd2545f3
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 06:00:00 +0300

    Final ttask

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Function that executes multiple coroutines."""
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> list[float]:
+    """Use multiple coroutines."""
+    # Create a list of asyncio.Task objects
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    # Gather the results as they complete
+    delays = await asyncio.gather(*tasks)
+
+    # Return the list of delays sorted in ascending order
+    return sorted(delays)
