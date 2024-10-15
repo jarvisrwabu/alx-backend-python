dcb9aadef881dda4f2e4c95b959d6e4623dc1b55
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 11:33:45 +0300

    Use List type annotation

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Function that executes multiple coroutines."""
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Use multiple coroutines."""
     # Create a list of asyncio.Task objects
     tasks = [task_wait_random(max_delay) for _ in range(n)]
