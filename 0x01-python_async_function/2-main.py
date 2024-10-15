bcef3c31c179e2721121f8ad42fdbfaf07554270
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 12:09:14 +0300

    Fix tester file

--- 0x01-python_async_function/2-main.py
+++ 0x01-python_async_function/2-main.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import asyncio
 measure_time = __import__('2-measure_runtime').measure_time
 
 n = 5
 max_delay = 9
 
-#print(measure_time(n, max_delay))
-print(asyncio.run(measure_time(n, max_delay)))
+print(measure_time(n, max_delay))
