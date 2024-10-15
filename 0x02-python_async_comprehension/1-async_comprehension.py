469427deaa06d7a4e2b065d719686f016c88ccd2
jarvisrwabu <jarvisrwabuyongo45@gmail.com>
2024-10-15 17:11:46 +0300

    Async comprehension

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Module for asynchronous random number collection.
+
+This module defines a coroutine that collects random float numbers
+between 0 and 10 using an asynchronous comprehension over a previously
+defined asynchronous generator.
+
+Functions:
+-----------
+- async_comprehension: Collects 10 random float numbers using
+  an asynchronous comprehension over the async_generator.
+
+Usage:
+------
+To execute the module, run it as a standalone script. The collected
+random numbers will be printed to the console.
+
+Example:
+-------
+$ python3 your_module_name.py
+
+"""
+import asyncio
+from typing import List
+
+# Import async_generator from the previous task
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    Collect 10 random numbers using an async comprehension.
+
+    over async_generator.
+    """
+    return [i async for i in async_generator()]
+
+
+async def main() -> None:
+    """Entry Point."""
+    print(await async_comprehension())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
